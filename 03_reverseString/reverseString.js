ef9094618b308c5c0936952a34e8e6cd3ba4177f
mukudanieru <111953203+mukudanieru@users.noreply.github.com>
2024-12-10 13:46:08 +0800

    Finish 03_reverseString
    I think the C programming language has rewired something in my brain. I'm not used to relying on existing methods. :((.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    let newString = "";
+    
+    let splittedText = text.split("");
 
+    for (let i = splittedText.length - 1; i >= 0; i--) {
+        newString += splittedText[i];
+    }
+
+    return newString;
 };
 
 // Do not edit below this line
