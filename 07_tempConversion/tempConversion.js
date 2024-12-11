3d58f80afee45d88c5a630b84109215371d37b99
mukudanieru <111953203+mukudanieru@users.noreply.github.com>
2024-12-11 15:36:20 +0800

    Finish 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahreinheit) {
+  let celsius = (5 * (fahreinheit - 32)) / 9;
+  return parseFloat(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahreinheit = (celsius * 9 / 5) + 32;
+  return parseFloat(fahreinheit.toFixed(1));
 };
 
 // Do not edit below this line
