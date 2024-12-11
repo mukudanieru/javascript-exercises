7d9ae0c89aa2a91ee6dad0db26851dbee0e3cde2
mukudanieru <111953203+mukudanieru@users.noreply.github.com>
2024-12-11 14:14:46 +0800

    Finish 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
+    if (!Number.isInteger(n1) || !Number.isInteger(n2)) {
+        return "ERROR";
+    }
 
+    if (n1 < 0 || n2 < 0) {
+        return "ERROR";
+    }
+
+    if (n1 > n2) [n1, n2] = [n2, n1];
+
+    let sum = 0;
+
+    for (let i = n1; i <= n2; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
+
+console.log(sumAll(1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
