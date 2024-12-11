fbbe560fc01be5bd83d8074d91059eef4a0fb71b
mukudanieru <111953203+mukudanieru@users.noreply.github.com>
2024-12-11 13:55:17 +0800

    Finish 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let newArr = [];
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (!args.includes(arr[i])) {
+            newArr.push(arr[i]);
+        }
+    }
+    
+    return newArr;
+    // return arr.filter((e) => !args.includes(e));
 };
+
+console.log(removeFromArray([1, 2, 2, 3], 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
