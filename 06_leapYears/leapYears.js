c57073ed410770f395864ea9038670debdd6090b
mukudanieru <111953203+mukudanieru@users.noreply.github.com>
2024-12-11 15:14:12 +0800

    Finish 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    return (year % 100 != 0 || year % 400 === 0) && year % 4 === 0;
 };
+
+console.log(leapYears(1984));
+console.log(leapYears(2004));
+console.log(leapYears(1800));
+console.log(leapYears(1900));
+console.log(leapYears(1600));
+console.log(leapYears(2000));
 
 // Do not edit below this line
 module.exports = leapYears;
