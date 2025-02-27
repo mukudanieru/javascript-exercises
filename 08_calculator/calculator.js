11d706f25bdafc069a54b7b4778c0e0d07f43100
mukudanieru <111953203+mukudanieru@users.noreply.github.com>
2025-02-27 18:42:58 +0800

    Finish 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,45 @@
-const add = function() {
-	
+const add = function (a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (listOfNumber) {
+    return listOfNumber.reduce((sum, number) => {
+        return sum + number;
+    }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (listOfNumber) {
+    return listOfNumber.reduce((product, number) => {
+        return product * number;
+    }, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+    return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+    let product = 1;
+
+    for (let i = a; i > 0; i--) {
+        product *= i;
+    }
+
+    return product;
 };
+
+console.log(factorial(10));
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial,
 };
