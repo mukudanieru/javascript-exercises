01daa193293dbbd80bf2404d08a03bd3f8ff59c8
mukudanieru <111953203+mukudanieru@users.noreply.github.com>
2025-03-01 09:32:25 +0800

    Finish 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const cleanedStr = str
+        .toLowerCase()
+        .replace(/[.,!?:;'"(){}\[\]<>-\s]/g, "");
 
+    const reversedString = cleanedStr.split("").reverse().join("");
+
+    return cleanedStr === reversedString;
 };
+
+console.log(palindromes("race Car!"));
 
 // Do not edit below this line
 module.exports = palindromes;
