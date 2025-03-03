78e9b0eb48e0ede3e04651b9bec7b7d2dd38fc8a
mukudanieru <111953203+mukudanieru@users.noreply.github.com>
2025-03-03 21:00:23 +0800

    Finish 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,21 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+    return books.map((book) => {
+        return book["title"];
+    });
 };
+
+const books = [
+    {
+        title: "Book",
+        author: "Name",
+    },
+    {
+        title: "Book2",
+        author: "Name2",
+    },
+];
+
+console.log(getTheTitles(books));
 
 // Do not edit below this line
 module.exports = getTheTitles;
