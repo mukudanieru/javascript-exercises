7685338995a3abe6bb3d874df9ba1178086f3014
mukudanieru <111953203+mukudanieru@users.noreply.github.com>
2025-03-03 21:48:28 +0800

    Finish 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-const findTheOldest = function() {
+function getAge(birth, death) {
+    const currentYearDate = new Date().getFullYear();
+    if (!death) {
+        death = currentYearDate;
+    }
 
+    return death - birth;
+}
+
+const findTheOldest = function (people) {
+    const theOldest = people.reduce((oldest, person) => {
+        // First person is automatically selected
+        if (oldest === null) {
+            return person;
+        }
+
+        let currentOldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        let currentPersonAge = getAge(person.yearOfBirth, person.yearOfDeath);
+
+        if (currentPersonAge > currentOldestAge) {
+            return person;
+        }
+
+        return oldest;
+    }, null);
+
+    return theOldest;
 };
 
 // Do not edit below this line
